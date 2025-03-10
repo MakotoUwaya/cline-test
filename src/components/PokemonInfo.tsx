eff8df04e88f8e11e58839931fa0e28586a67409
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2025-03-10 19:32:26 +0900

    style: improve pokemon card layout and grid responsiveness

--- src/components/PokemonInfo.tsx
+++ src/components/PokemonInfo.tsx
@@ -6,14 +6,9 @@ interface PokemonInfoProps {
 
 export const PokemonInfo = ({ pokemon }: PokemonInfoProps) => {
   return (
-    <div>
-      <h1>{pokemon.name} Information</h1>
-      <div className="card">
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-        <p>Name: {pokemon.name}</p>
-        <p>Height: {pokemon.height}</p>
-        <p>Weight: {pokemon.weight}</p>
-      </div>
+    <div className="pokemon-info">
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      <p>{pokemon.name}</p>
     </div>
   );
 };
